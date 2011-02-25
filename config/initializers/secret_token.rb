5abf7e8200fb5d3ac7439ce833c9683e6dc1842f
Ivan Zvonkov <arrrght@gmail.com>
2011-02-25 18:49:13 +0500

    rails new netzke-test-1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeTest1::Application.config.secret_token = '22b1660e32f443a4bcd79dd31897ad83825d14deab5338b37d6ec658fb1306d6a9cad9dcf6d774970b246411ae5a7469b06d53857d747e60892e149ff2366858'
